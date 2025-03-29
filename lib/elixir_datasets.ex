9c7fe79774d7a65f0b6d9a3892fdebff65a892ab
Radosław Rolka <radoslaw.rolka@gmail.com>
2025-03-29 18:14:33 +0100

    init library

---
+++ lib/elixir_datasets.ex
@@ -0,0 +1,18 @@
+defmodule ElixirDatasets do
+  @moduledoc """
+  Documentation for `ElixirDatasets`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirDatasets.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixir_datasets_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirDatasetsTest do
+  use ExUnit.Case
+  doctest ElixirDatasets
+
+  test "greets the world" do
+    assert ElixirDatasets.hello() == :world
+  end
+end
