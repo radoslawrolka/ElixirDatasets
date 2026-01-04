057dc3a96be8800fcebe2c1eae793bcfda299051
Radosław Rolka <radoslaw.rolka@gmail.com>
2026-01-04 12:20:59 +0100

    [FEATURE] Basic deletion files from hub

--- lib/elixir_datasets/utils/uploader.ex
+++ lib/elixir_datasets/utils/uploader.ex
@@ -31,6 +31,40 @@ defmodule ElixirDatasets.Utils.Uploader do
   end
 
   @doc """
+  Deletes a file from a specified Hugging Face dataset repository.
+
+  ## Parameters
+    - `repository`: The Hugging Face repository path (e.g., "username/dataset-name")
+    - `filename`: The path of the file to delete in the repository
+    - `options`: A keyword list with optional parameters:
+      - `:commit_message`: Custom commit message (default: "Delete file from ElixirDatasets")
+      - `:description`: Optional description for the commit
+
+  ## Examples
+      iex> delete_file_from_dataset("username/dataset", "old_file.csv", [])
+      {:ok, response_body}
+
+      iex> delete_file_from_dataset("username/dataset", "data.csv",
+      ...>   commit_message: "Removing outdated data",
+      ...>   description: "Data no longer needed"
+      ...> )
+      {:ok, response_body}
+  """
+  @spec delete_file_from_dataset(String.t(), String.t(), keyword()) ::
+          {:ok, String.t()} | {:error, String.t() | Exception.t()}
+  def delete_file_from_dataset(repository, filename, options \\ []) when is_binary(repository) and is_binary(filename) do
+    commit_msg = Keyword.get(options, :commit_message, "Delete file from ElixirDatasets")
+    description = Keyword.get(options, :description, "")
+
+    with {:ok, token} <- get_hf_token(),
+         {:ok, response} <- delete_from_huggingface(repository, token, filename, commit_msg, description) do
+      {:ok, response}
+    else
+      {:error, reason} -> {:error, reason}
+    end
+  end
+
+  @doc """
   Verifies that the provided options for uploading are valid.
   """
   @spec verify_options!(keyword()) :: :ok | no_return()
@@ -89,6 +123,43 @@ defmodule ElixirDatasets.Utils.Uploader do
     })
 
     body = "#{header_line}\n#{file_line}"
+
+    headers = [
+      {~c"Authorization", ~c"Bearer #{token}"},
+      {~c"Content-Type", ~c"application/x-ndjson"}
+    ]
+
+    case :httpc.request(:post, {String.to_charlist(url), headers, ~c"application/x-ndjson", String.to_charlist(body)}, [], []) do
+      {:ok, {{_protocol, status_code, _message}, _headers, response_body}} when status_code in 200..299 ->
+        {:ok, response_body}
+      {:ok, {{_protocol, status_code, _message}, _headers, response_body}} ->
+        {:error, "HTTP Error #{status_code}: #{response_body}"}
+      {:error, reason} ->
+        {:error, reason}
+    end
+  end
+
+  @doc false
+  defp delete_from_huggingface(repository, token, filename, commit_msg, description) do
+    url = "#{@huggingface_endpoint}/api/datasets/#{repository}/commit/main"
+
+    # Prepare NDJSON payload
+    header_line = Jason.encode!(%{
+      "key" => "header",
+      "value" => %{
+        "summary" => commit_msg,
+        "description" => description
+      }
+    })
+
+    deleted_file_line = Jason.encode!(%{
+      "key" => "deletedFile",
+      "value" => %{
+        "path" => filename
+      }
+    })
+
+    body = "#{header_line}\n#{deleted_file_line}"
 
     headers = [
       {~c"Authorization", ~c"Bearer #{token}"},
